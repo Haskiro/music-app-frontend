9c33420328aec4a7c6d3785eb76022b791d8fd64
Павел <58857483+Haskiro@users.noreply.github.com>
2023-01-09 13:25:22 +0300

    Main header component, routing created

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,34 @@
+import { HeaderMain } from "@components/HeaderMain/HeaderMain";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 function App() {
-	return <div className="App"></div>;
+	return (
+		<div className="App">
+			<BrowserRouter>
+				<Routes>
+					<Route path="/" element={<HeaderMain />} />
+					<Route path="/tracks" element={<>tracks</>} />
+					<Route path="/artists" element={<>artists</>} />
+					<Route path="/genres" element={<>genres</>} />
+					<Route path="/playlists" element={<>playlists</>} />
+					<Route
+						path="*"
+						element={
+							<h1
+								style={{
+									fontSize: "30px",
+									textAlign: "center",
+									marginTop: "20px",
+								}}
+							>
+								404 Page Not Found
+							</h1>
+						}
+					/>
+				</Routes>
+			</BrowserRouter>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/HeaderMain/HeaderMain.tsx
@@ -0,0 +1,95 @@
+import { FC, useState } from "react";
+import Burger from "@assets/icons/burger.svg";
+import cn from "classnames";
+import { NavLink } from "react-router-dom";
+import styles from "./Header.module.scss";
+import { HeaderProps } from "./Header.props";
+
+export const HeaderMain: FC<HeaderProps> = () => {
+	const [navIsActive, setNavIsActive] = useState<boolean>(false);
+	return (
+		<header className={styles.block}>
+			<div className={cn(styles.body, "container")}>
+				<div className={styles.user}>
+					<p className={styles.text}>Павел</p>
+					<button className={styles.button}>Выйти</button>
+				</div>
+				<nav
+					className={cn(styles.nav, {
+						[styles.navActive]: navIsActive,
+					})}
+				>
+					<ul className={styles.list}>
+						<li className={styles.item}>
+							<NavLink
+								to="/"
+								end
+								className={({ isActive }) =>
+									cn(styles.link, {
+										[styles.linkActive]: isActive,
+									})
+								}
+							>
+								Профиль
+							</NavLink>
+						</li>
+						<li className={styles.item}>
+							<NavLink
+								to="/tracks"
+								className={({ isActive }) =>
+									cn(styles.link, {
+										[styles.linkActive]: isActive,
+									})
+								}
+							>
+								Треки
+							</NavLink>
+						</li>
+						<li className={styles.item}>
+							<NavLink
+								to="/artists"
+								className={({ isActive }) =>
+									cn(styles.link, {
+										[styles.linkActive]: isActive,
+									})
+								}
+							>
+								Исполнители
+							</NavLink>
+						</li>
+						<li className={styles.item}>
+							<NavLink
+								to="/genres"
+								className={({ isActive }) =>
+									cn(styles.link, {
+										[styles.linkActive]: isActive,
+									})
+								}
+							>
+								Жанры
+							</NavLink>
+						</li>
+						<li className={styles.item}>
+							<NavLink
+								to="/playlists"
+								className={({ isActive }) =>
+									cn(styles.link, {
+										[styles.linkActive]: isActive,
+									})
+								}
+							>
+								Плейлисты
+							</NavLink>
+						</li>
+					</ul>
+				</nav>
+				<button
+					className={styles.burger}
+					onClick={() => setNavIsActive((prevState) => !prevState)}
+				>
+					<img src={Burger} alt="Меню" />
+				</button>
+			</div>
+		</header>
+	);
+};
