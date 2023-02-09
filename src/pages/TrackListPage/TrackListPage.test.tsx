78b908bdb3ff005790dd6c401d44b65802ac2d14
Павел <58857483+Haskiro@users.noreply.github.com>
2023-02-09 15:29:04 +0300

    dispatch and actions fetchTracks tests

--- src/pages/TrackListPage/TrackListPage.test.tsx
+++ src/pages/TrackListPage/TrackListPage.test.tsx
@@ -20,16 +20,19 @@ jest.mock("../../store/hooks");
 
 const mockedUseAppSelector = jest.spyOn(reduxHooks, "useAppSelector");
 const mockedUseAppDispatch = jest.spyOn(reduxHooks, "useAppDispatch");
+const mockedFetchTracks = jest.spyOn(actions, "fetchTracks");
 
 describe("Track List Page", () => {
 	it("should create Track list page with empty tracks", () => {
 		// Only js files, no typescript
 		// mockedUseAppSelector.mockReturnValue([]);
-
+		const dispatch = jest.fn();
 		mockedUseAppSelector.mockReturnValue([]);
-		mockedUseAppDispatch.mockReturnValue(jest.fn());
+		mockedUseAppDispatch.mockReturnValue(dispatch);
 		const component = render(<TrackListPage />);
 
 		expect(component).toMatchSnapshot();
+		expect(dispatch).toHaveBeenCalledTimes(1);
+		expect(mockedFetchTracks).toHaveBeenCalledTimes(1);
 	});
 });
