3493b1296e39dad035ddd472cdbe9bbc71103fb0
Павел <58857483+Haskiro@users.noreply.github.com>
2023-02-12 00:19:02 +0300

    userSlice extraReducers tests

--- src/__tests__/store/slices/userSlice.test.ts
+++ src/__tests__/store/slices/userSlice.test.ts
@@ -1,7 +1,5 @@
 import { IAccessToken, IUser } from "@interfaces/user.interface";
-import { RootState } from "@store/store";
 import axios from "axios";
-import { PersistPartial } from "redux-persist/es/persistReducer";
 import userReducer, {
 	logout,
 	editUser,
@@ -11,6 +9,7 @@ import userReducer, {
 	IRegisterData,
 	registerUser,
 	fetchUser,
+	updateUser,
 } from "../../../store/slices/userSlice";
 
 jest.mock("axios");
@@ -102,6 +101,33 @@ describe("User Reducer", () => {
 			expect(end[0].type).toBe("user/authUser/rejected");
 			expect(end[0].error.message).toBe("fetch error");
 		});
+
+		describe("Extra reducers", () => {
+			it('should change authStatus if "authUser.pending" action', () => {
+				const state = userReducer(initialState, {
+					type: "user/authUser/pending",
+				});
+
+				expect(state.authStatus).toBe("loading");
+			});
+
+			it('should change authStatus and accessToken if "authUser.fulfilled" action', () => {
+				const state = userReducer(initialState, {
+					type: "user/authUser/fulfilled",
+					payload: { access: "accessToken" },
+				});
+
+				expect(state.accessToken).toBe("accessToken");
+				expect(state.authStatus).toBe("succeeded");
+			});
+			it('should change authStatus if "authUser.rejected" action', () => {
+				const state = userReducer(initialState, {
+					type: "user/authUser/rejected",
+				});
+
+				expect(state.authStatus).toBe("failed");
+			});
+		});
 	});
 
 	describe("Register User Thunk", () => {
@@ -133,7 +159,7 @@ describe("User Reducer", () => {
 			expect(end[0].payload).toStrictEqual(mockToken);
 		});
 
-		it("should authUser with resolved response", async () => {
+		it("should registerUser with rejected response", async () => {
 			mockedAxios.post.mockImplementation(() =>
 				Promise.reject("fetch error")
 			);
@@ -150,18 +176,45 @@ describe("User Reducer", () => {
 			expect(end[0].type).toBe("user/registerUser/rejected");
 			expect(end[0].error.message).toBe("fetch error");
 		});
+
+		describe("Extra reducers", () => {
+			it('should change authStatus if "registerUser.pending" action', () => {
+				const state = userReducer(initialState, {
+					type: "user/registerUser/pending",
+				});
+
+				expect(state.authStatus).toBe("loading");
+			});
+
+			it('should change authStatus and accessToken if "registerUser.fulfilled" action', () => {
+				const state = userReducer(initialState, {
+					type: "user/registerUser/fulfilled",
+					payload: { access: "accessToken" },
+				});
+
+				expect(state.accessToken).toBe("accessToken");
+				expect(state.authStatus).toBe("succeeded");
+			});
+			it('should change authStatus if "registerUser.rejected" action', () => {
+				const state = userReducer(initialState, {
+					type: "user/registerUser/rejected",
+				});
+
+				expect(state.authStatus).toBe("failed");
+			});
+		});
 	});
 
 	describe("Fetch User Thunk", () => {
-		it("should fetchUser with resolved response", async () => {
-			const mockUser: IUser = {
-				first_name: "Pavel",
-				last_name: "Surname",
-				bio: "",
-				email: "email@email.email",
-				photo: "",
-			};
+		const mockUser: IUser = {
+			first_name: "Pavel",
+			last_name: "Surname",
+			bio: "",
+			email: "email@email.email",
+			photo: "",
+		};
 
+		it("should fetchUser with resolved response", async () => {
 			mockedAxios.get.mockImplementation(() =>
 				Promise.resolve({ data: mockUser })
 			);
@@ -209,6 +262,125 @@ describe("User Reducer", () => {
 			expect(start[0].type).toBe("user/fetchUser/pending");
 			expect(end[0].type).toBe("user/fetchUser/rejected");
 			expect(end[0].error.message).toBe("fetch error");
+		});
+
+		describe("Extra reducers", () => {
+			it('should change status if "fetchUser.pending" action', () => {
+				const state = userReducer(initialState, {
+					type: "user/fetchUser/pending",
+				});
+
+				expect(state.status).toBe("loading");
+			});
+
+			it('should change status and user if "fetchUser.fulfilled" action', () => {
+				const state = userReducer(initialState, {
+					type: "user/fetchUser/fulfilled",
+					payload: mockUser,
+				});
+
+				expect(state.user).toStrictEqual(mockUser);
+				expect(state.status).toBe("succeeded");
+			});
+			it('should change status if "fetchUser.rejected" action', () => {
+				const state = userReducer(initialState, {
+					type: "user/fetchUser/rejected",
+				});
+
+				expect(state.status).toBe("failed");
+			});
+		});
+	});
+
+	describe("Update User Thunk", () => {
+		const currentUser: IUser = {
+			first_name: "Pavel",
+			last_name: "Surname",
+			bio: "",
+			email: "email@email.email",
+			photo: "",
+		};
+
+		it("should update with resolved response", async () => {
+			mockedAxios.patch.mockImplementation(() =>
+				Promise.resolve({ data: { message: "user updated" } })
+			);
+			const dispatch = jest.fn();
+			const thunk = updateUser({ ...currentUser, bio: "New Biography" });
+			const getState = jest.fn().mockReturnValue({
+				user: {
+					user: currentUser,
+					accessToken: "accessToken",
+					status: "succeeded",
+					authStatus: "succeeded",
+				},
+			});
+
+			await thunk(dispatch, getState, undefined);
+			const { calls } = dispatch.mock;
+
+			expect(calls).toHaveLength(3);
+
+			expect(calls[0][0].type).toBe("user/updateUser/pending");
+			expect(calls[1][0].type).toBe(editUser.type);
+			expect(calls[1][0].payload).toStrictEqual({
+				...currentUser,
+				bio: "New Biography",
+			});
+			expect(calls[2][0].type).toBe("user/updateUser/fulfilled");
+			expect(calls[2][0].payload).toStrictEqual({
+				message: "user updated",
+			});
+		});
+
+		it("should updateUser with rejected response", async () => {
+			mockedAxios.patch.mockImplementation(() =>
+				Promise.reject("fetch error")
+			);
+			const dispatch = jest.fn();
+			const thunk = updateUser({ ...currentUser, bio: "New Biography" });
+			const getState = jest.fn().mockReturnValue({
+				user: {
+					user: currentUser,
+					accessToken: "accessToken",
+					status: "succeeded",
+					authStatus: "succeeded",
+				},
+			});
+
+			await thunk(dispatch, getState, undefined);
+			const { calls } = dispatch.mock;
+
+			expect(calls).toHaveLength(2);
+			expect(calls[0][0].type).toBe("user/updateUser/pending");
+			expect(calls[1][0].type).toBe("user/updateUser/rejected");
+			expect(calls[1][0].error.message).toBe("fetch error");
+		});
+
+		describe("Extra reducers", () => {
+			it('should change status if "updateUser.pending" action', () => {
+				const state = userReducer(initialState, {
+					type: "user/updateUser/pending",
+				});
+
+				expect(state.status).toBe("loading");
+			});
+
+			it('should change status if "updateUser.fulfilled" action', () => {
+				const state = userReducer(initialState, {
+					type: "user/updateUser/fulfilled",
+					payload: { message: "user updated" },
+				});
+
+				expect(state.status).toBe("succeeded");
+			});
+			it('should change status if "updateUser.rejected" action', () => {
+				const state = userReducer(initialState, {
+					type: "user/updateUser/rejected",
+				});
+
+				expect(state.status).toBe("failed");
+			});
 		});
 	});
 });
